609e96e053d2a6e510afb6ce8dfe690b3d31ab84
Michael Adrian <michaeladrian39@gmail.com>
2017-08-26 18:48:45 +0700

    GUI in progress

--- Calcudoku/src/view/GUIGrid.java
+++ Calcudoku/src/view/GUIGrid.java
@@ -10,19 +10,15 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.HashMap;
-import java.util.Map;
 import javax.swing.BorderFactory;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
 import model.Cage;
 import model.Cell;
 
@@ -41,12 +37,11 @@ public class GUIGrid extends JPanel
     private final Cell[][] grid;
     private final Cage[] cages;
     private final JTextField[][] textFields;
-    private final Map<JTextField, Point> textFieldCoordinates = new HashMap<>();
     private static final Font FONT = new Font("Courier New", 
             Font.CENTER_BASELINE, 24);
     private final int cellSize = 48;
     private final int cellBorderWidth = 1;
-    private final int cageBorderWidth = 1;
+    private final int cageBorderWidth = 2;
     private Dimension textFieldDimension;
     
     public GUIGrid(Controller c)
@@ -108,7 +103,6 @@ public class GUIGrid extends JPanel
             {
                 JTextField textField = new JTextField();
                 textField.addKeyListener(new SudokuCellKeyListener(this));
-                textFieldCoordinates.put(textField, new Point(x, y));
                 textFields[x][y] = textField;
             }   
         }
@@ -117,8 +111,101 @@ public class GUIGrid extends JPanel
             for (int x = 0; x < size; x++)
             {
                 JTextField textField = textFields[y][x];
-                textField.setBorder(BorderFactory.createLineBorder(Color.BLACK, 
-                        cellBorderWidth));
+                int topBorderWidth;
+                int leftBorderWidth;
+                int bottomBorderWidth;
+                int rightBorderWidth;
+                if (y == 0)
+                {
+                    topBorderWidth = cageBorderWidth;
+                    if (cageCells[y][x] == cageCells[y + 1][x])
+                    {
+                        bottomBorderWidth = cellBorderWidth;
+                    }
+                    else
+                    {
+                        bottomBorderWidth = cageBorderWidth;
+                    }
+                }
+                else if (y == size - 1)
+                {
+                    bottomBorderWidth = cageBorderWidth;
+                    if (cageCells[y][x] == cageCells[y - 1][x])
+                    {
+                        topBorderWidth = cellBorderWidth;
+                    }
+                    else
+                    {
+                        topBorderWidth = cageBorderWidth;
+                    }
+                }
+                else
+                {
+                    if (cageCells[y][x] == cageCells[y + 1][x])
+                    {
+                        bottomBorderWidth = cellBorderWidth;
+                    }
+                    else
+                    {
+                        bottomBorderWidth = cageBorderWidth;
+                    }
+                    if (cageCells[y][x] == cageCells[y - 1][x])
+                    {
+                        topBorderWidth = cellBorderWidth;
+                    }
+                    else
+                    {
+                        topBorderWidth = cageBorderWidth;
+                    }
+                }
+                if (x == 0)
+                {
+                    leftBorderWidth = cageBorderWidth;
+                    if (cageCells[y][x] == cageCells[y][x + 1])
+                    {
+                        rightBorderWidth = cellBorderWidth;
+                    }
+                    else
+                    {
+                        rightBorderWidth = cageBorderWidth;
+                    }
+                }
+                else if (x == size - 1)
+                {
+                    rightBorderWidth = cageBorderWidth;
+                    if (cageCells[y][x] == cageCells[y][x - 1])
+                    {
+                        leftBorderWidth = cellBorderWidth;
+                    }
+                    else
+                    {
+                        leftBorderWidth = cageBorderWidth;
+                    }
+                }
+                else
+                {
+                    if (cageCells[y][x] == cageCells[y][x + 1])
+                    {
+                        rightBorderWidth = cellBorderWidth;
+                    }
+                    else
+                    {
+                        rightBorderWidth = cageBorderWidth;
+                    }
+                    if (cageCells[y][x] == cageCells[y][x - 1])
+                    {
+                        leftBorderWidth = cellBorderWidth;
+                    }
+                    else
+                    {
+                        leftBorderWidth = cageBorderWidth;
+                    }
+                }
+                textField.setToolTipText(
+                        cages[grid[y][x].getCageID()].getObjective());
+                textField.setBorder(BorderFactory.createMatteBorder(
+                        topBorderWidth, leftBorderWidth, bottomBorderWidth, 
+                        rightBorderWidth, Color.BLACK));
                 textField.setFont(FONT);
                 textField.setHorizontalAlignment(JTextField.CENTER);
                 textField.setPreferredSize(textFieldDimension);                
@@ -142,79 +229,6 @@ public class GUIGrid extends JPanel
             }
         }
     }
-    
-    public void moveCursor(JTextField textField, int keyCode)
-    {
-        Point coordinates = textFieldCoordinates.get(textField);
-        switch (keyCode)
-        {
-            case KeyEvent.VK_LEFT:
-                if (coordinates.x > 0)
-                {
-                    textFields[coordinates.y][coordinates.x - 1].requestFocus();
-                    addSpace(textFields[coordinates.y][coordinates.x - 1]);
-                }
-                break;
-            case KeyEvent.VK_KP_LEFT:
-                if (coordinates.x > 0)
-                {
-                    textFields[coordinates.y][coordinates.x - 1].requestFocus();
-                    addSpace(textFields[coordinates.y][coordinates.x - 1]);
-                }
-                break;
-            case KeyEvent.VK_UP:
-                if (coordinates.y > 0)
-                {
-                    textFields[coordinates.y - 1][coordinates.x].requestFocus();
-                    addSpace(textFields[coordinates.y - 1][coordinates.x]);
-                }
-                break;
-            case KeyEvent.VK_KP_UP:
-                if (coordinates.y > 0)
-                {
-                    textFields[coordinates.y - 1][coordinates.x].requestFocus();
-                    addSpace(textFields[coordinates.y - 1][coordinates.x]);
-                }
-                break;
-            case KeyEvent.VK_RIGHT:
-                if (coordinates.x < size - 1)
-                {
-                    textFields[coordinates.y][coordinates.x + 1].requestFocus();
-                    addSpace(textFields[coordinates.y][coordinates.x + 1]);
-                }
-                break;
-            case KeyEvent.VK_KP_RIGHT:
-                if (coordinates.x < size - 1)
-                {
-                    textFields[coordinates.y][coordinates.x + 1].requestFocus();
-                    addSpace(textFields[coordinates.y][coordinates.x + 1]);
-                }
-                break;
-            case KeyEvent.VK_DOWN:
-                if (coordinates.y < size - 1)
-                {
-                    textFields[coordinates.y + 1][coordinates.x].requestFocus();
-                    addSpace(textFields[coordinates.y + 1][coordinates.x]);
-                }
-                break;
-            case KeyEvent.VK_KP_DOWN:
-                if (coordinates.y < size - 1)
-                {
-                    textFields[coordinates.y + 1][coordinates.x].requestFocus();
-                    addSpace(textFields[coordinates.y + 1][coordinates.x]);
-                }
-                break;
-        }
-    }
-    
-    private void addSpace(JTextField textField)
-    {
-        if (textField.getText().isEmpty())
-        {
-            textField.setText(" ");
-        }
-    }
-    
 }
 
 class SudokuCellKeyListener implements KeyListener
@@ -230,49 +244,12 @@ class SudokuCellKeyListener implements KeyListener
     @Override
     public void keyTyped(KeyEvent e)
     {
-        int keyCode = e.getKeyCode();
         JTextField textField = (JTextField) e.getSource();        
-        switch (keyCode)
+        String gridSize = "" + gui.getGridSize();
+        int gridSizeDigits = gridSize.length();
+        if (textField.getText().length() >= gridSizeDigits)
         {
-            case KeyEvent.VK_LEFT :
-                e.consume();
-                gui.moveCursor(textField, KeyEvent.VK_LEFT);
-                break;
-            case KeyEvent.VK_UP :
-                e.consume();
-                gui.moveCursor(textField, KeyEvent.VK_UP);
-                break;
-            case KeyEvent.VK_RIGHT :
-                e.consume();
-                gui.moveCursor(textField, KeyEvent.VK_RIGHT);
-                break;
-            case KeyEvent.VK_DOWN :
-                e.consume();
-                gui.moveCursor(textField, KeyEvent.VK_DOWN);
-                break;
-            case KeyEvent.VK_KP_LEFT :
-                e.consume();
-                gui.moveCursor(textField, KeyEvent.VK_KP_LEFT);
-                break;
-            case KeyEvent.VK_KP_UP :
-                e.consume();
-                gui.moveCursor(textField, KeyEvent.VK_KP_UP);
-                break;
-            case KeyEvent.VK_KP_RIGHT :
-                e.consume();
-                gui.moveCursor(textField, KeyEvent.VK_KP_RIGHT);
-                break;
-            case KeyEvent.VK_KP_DOWN :
-                e.consume();
-                gui.moveCursor(textField, KeyEvent.VK_KP_DOWN);
-                break;
-            default :
-                String gridSize = "" + gui.getGridSize();
-                int gridSizeDigits = gridSize.length();
-                if (textField.getText().length() >= gridSizeDigits)
-                {
-                    e.consume();
-                }
+            e.consume();
         }
     }
 
@@ -309,14 +286,3 @@ class PopupMenuListener implements ActionListener
     }
     
 }
-
-//            this.objectivePositionRow = new int[numberOfCages];
-//            this.objectivePositionColumn = new int[numberOfCages];
-//            Cage[] cages = c.getCages();
-//            for (int i = 0; i < numberOfCages; i++)
-//            {
-//                this.objectivePositionRow[i] = 
-//                        cages[i].getCells().get(0).getRow();
-//                this.objectivePositionColumn[i] = 
-//                        cages[i].getCells().get(0).getColumn();
-//            }
