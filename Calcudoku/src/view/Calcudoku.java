7b00150346ca0d0562e697c82dac921d93a65198
Michael Adrian <michaeladrian39@gmail.com>
2017-09-04 19:45:59 +0700

    add puzzle files + fix bugs

--- Calcudoku/src/view/Calcudoku.java
+++ Calcudoku/src/view/Calcudoku.java
@@ -323,6 +323,13 @@ public class Calcudoku extends JFrame
     private void resetFrame()
     {
         this.getContentPane().removeAll();
+        this.puzzleFile = null;
+        this.puzzleFileName = null;
+        this.size = 0;
+        this.cageCells = null;
+        this.numberOfCages = 0;
+        this.cageObjectives = null;
+        this.c = null;
         this.setTitle("Calcudoku");
         this.validate();
         this.revalidate();
